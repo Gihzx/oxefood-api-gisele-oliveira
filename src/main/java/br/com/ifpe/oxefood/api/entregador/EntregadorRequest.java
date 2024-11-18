cc3a9161f321f4179e0a5efdaf74cc96acc5bf31
Gisele <giseleoliveiradasilva2022@gmail.com>
2024-11-18 19:46:27 -0300

    fix: mudando datNacimento

--- src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorRequest.java
@@ -3,6 +3,7 @@ package br.com.ifpe.oxefood.api.entregador;
 import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import br.com.ifpe.oxefood.modelo.entregador.Entregador;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,9 +17,9 @@ import lombok.NoArgsConstructor;
 public class EntregadorRequest {
       private String nome;
     // formata fomato de data 
-   @JsonFormat(pattern = "dd/MM/yyyy")
+
    private String cpf;
-   
+   @JsonFormat(pattern = "dd/MM/yyyy")
    private LocalDate dataNascimento;
 
    private String rg;
