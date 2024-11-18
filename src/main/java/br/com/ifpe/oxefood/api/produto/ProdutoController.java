311b1eeab3e9caf3f806df9b227949ba9e681d7a
Gihzx <145726882+Gihzx@users.noreply.github.com>
2024-11-18 17:04:48 -0300

    fix: correção de import produtoContoller

--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import br.com.ifpe.oxefood.api.produto.ProdutoRequest;
 import br.com.ifpe.oxefood.modelo.produto.Produto;
 import br.com.ifpe.oxefood.modelo.produto.ProdutoService;
 
