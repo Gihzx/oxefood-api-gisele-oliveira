6698a321191c593ef1012354b4b9a95658be42e4
Seu Nome <seuemail@dominio.com>
2024-12-16 19:41:36 -0300

    feat: controle de acesso

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteRequest.java
@@ -1,15 +1,20 @@
 package br.com.ifpe.oxefood.api.cliente;
+
 import java.time.LocalDate;
+import java.util.Arrays;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.br.CPF;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotBlank;
-
-import jakarta.validation.constraints.NotNull;  
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+import br.com.ifpe.oxefood.modelo.acesso.Perfil;
+import br.com.ifpe.oxefood.modelo.acesso.Usuario;
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,6 +27,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClienteRequest {
+    @NotBlank(message = "O e-mail é de preenchimento obrigatório")
+    @Email
+    private String email;
+
+    @NotBlank(message = "A senha é de preenchimento obrigatório")
+    private String password;
+
     // verifica se o campo não é nullo
       @NotNull(message = "O Nome é de preenchimento obrigatório")
       // verifica se o campo não é vazio
@@ -46,9 +58,20 @@ public class ClienteRequest {
 
    private String foneFixo;
 
+
+     public Usuario buildUsuario() {
+       return Usuario.builder()
+           .username(email)
+           .password(password)
+           .roles(Arrays.asList(new Perfil(Perfil.ROLE_CLIENTE)))
+           .build();
+   }
+
+
    public Cliente build() {
 
        return Cliente.builder()
+           .usuario(buildUsuario())
            .nome(nome)
            .dataNascimento(dataNascimento)
            .cpf(cpf)
